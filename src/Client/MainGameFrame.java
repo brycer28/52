fe1e7181e7cb5967a7d9fe39cbb894e7ab656073
Thi-Huynh01 <huynhthi265@gmail.com>
2025-04-01 22:28:42 -0500

    created MainGameFrame

---
+++ src/Client/MainGameFrame.java
@@ -0,0 +1,18 @@
+package Client;
+
+import javax.swing.*;
+import graphics.*;
+
+public class MainGameFrame extends JFrame {
+    private GameClient client;
+    private JPanel LoginPanel;
+    private JPanel AccountCreationPanel;
+    private JPanel LobbyPanel;
+    private JPanel GamePlayPanel;
+    private JPanel StatsPanel;
+    private JPanel CardPanel;
+
+    public void setPanel(JPanel panel) {}
+
+
+}
